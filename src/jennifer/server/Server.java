180cd65bee7c0aefd137f59e77f3398e4823f00e
Jennifer Ubah <cju.cipher@gmail.com>
2019-03-14 01:47:06 -0700

    Added Response Parser

--- src/jennifer/server/Server.java
+++ src/jennifer/server/Server.java
@@ -1,9 +1,6 @@
 package jennifer.server;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -26,7 +23,7 @@ public class Server {
 
     private static class RequestHandler extends Thread {
 
-        private PrintWriter writer;
+        private DataOutputStream writer;
         private BufferedReader reader;
         private Socket connSocket;
 
@@ -38,24 +35,38 @@ public class Server {
 
             try{
 
-                writer = new PrintWriter(connSocket.getOutputStream(), true);
+                writer = new DataOutputStream(connSocket.getOutputStream());
                 reader = new BufferedReader(new InputStreamReader(connSocket.getInputStream()));
 
-                String request;
-                StringBuffer buffer = new StringBuffer();
-                while((request = reader.readLine()) != null){
-                    if(request.isEmpty()){
+/*                String line;
+                String[] request = new String[5];
+                int count = 0;
+
+                while((line = reader.readLine()) != null){
+                    if(line.isEmpty()){
                         break;
                     }
-                    buffer.append(request);
-                    System.out.println(request);
-                }
 
-                writer.println("Hello World");
+                    request[count] = line;
+                    count++;
 
-                writer.close();
+                }*/
+
+                String request = reader.readLine();
+
+                //parse the request
+                String page = parseRequest(request);
+                System.out.println("my page " + page);
+
+                // send response based on reques
+                sendResponse(page);
+
+//                byte[] byteRes = "Hello World".getBytes();
+//                writer.write(byteRes);
+
+                //writer.close();
                 reader.close();
-                connSocket.close();
+                //connSocket.close();
 
             } catch (IOException e){
                 e.printStackTrace();
@@ -69,5 +80,43 @@ public class Server {
                 e.printStackTrace();
             }*/
         }
+
+        public String parseRequest(String request){
+
+            String[] firstLine = request.split(" ");
+            String page = firstLine[1];
+            return page;
+        }
+
+        public void sendResponse(String page){
+
+            if(page.equals("/index")){
+
+                String file = "src/jennifer/server/resources/index.html";
+                File path = new File(file);
+                String absolutePath = path.getAbsolutePath();
+                //System.out.println(absolutePath);
+                try(FileInputStream fileReader = new FileInputStream(absolutePath);
+                    BufferedOutputStream byteWriter = new BufferedOutputStream(new DataOutputStream(connSocket.getOutputStream()))){
+
+                    int byteRead;
+                    byte[] buffer = new byte[1024];
+                    while((byteRead = fileReader.read(buffer)) != -1){
+                        byteWriter.write(buffer, 0, byteRead);
+
+                    }
+
+
+
+                } catch (FileNotFoundException e){
+                    System.out.println("File not found");
+                } catch (IOException e){
+                    e.printStackTrace();
+                    System.out.println("something went wrong while reading file");
+                }
+            }
+
+        }
+
     }
 }
