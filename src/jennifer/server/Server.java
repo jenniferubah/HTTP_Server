678a1947939db6323c240ca07ddfe9d27d79ff36
Jennifer Ubah <cju.cipher@gmail.com>
2019-03-18 19:12:04 -0700

    Edited

--- src/jennifer/server/Server.java
+++ src/jennifer/server/Server.java
@@ -14,11 +14,16 @@ public class Server {
 
         try{
 
+            // creates a server socket with port number {port}
             serverSocket = new ServerSocket(port);
 
             while(true){
+                // creates a connection socket to listen and accept
+                // requests from clients
                 Socket connSocket = serverSocket.accept();
                 count++;
+
+                //creates a new thread to handle requests
                 new RequestHandler(connSocket).start();
             }
 
@@ -27,8 +32,8 @@ public class Server {
         }
     }
 
-    private static class RequestHandler extends Thread {
 
+    private static class RequestHandler extends Thread {
 
         private Socket connSocket;
 
@@ -41,18 +46,16 @@ public class Server {
             try{
 
                 BufferedReader reader = new BufferedReader(new InputStreamReader(connSocket.getInputStream()));
-
                 String request = reader.readLine();
 
                 // parse the request
                 String file = parseRequest(request);
-                System.out.println("my page " + file);
+                System.out.println("requesting... " + file);
+                String[] fileExtension = file.split("\\.");
+                System.out.println(fileExtension[1]);
 
                 // send response based on request
-                processRequest(file);
-
-
-
+                processRequest(file, fileExtension[1]);
 
             } catch (IOException e){
                 e.printStackTrace();
@@ -68,7 +71,7 @@ public class Server {
             return filePath;
         }
 
-        public void processRequest(String file){
+        public void processRequest(String file, String fileExt){
 
             //check if path exits
             String path = "src/jennifer/server/resources" + file;
@@ -80,58 +83,62 @@ public class Server {
 
                 String contentLength = Long.toString(rootPath.length());
                 System.out.println(contentLength);
-                String[] headers = constructHeader("HTTP/1.1 200 OK", contentLength);
+                String[] headers = constructHeader("HTTP/1.1 200 OK", contentLength, fileExt);
                 sendResponse(headers, rootPath);
             }
             else{
-
-                 path = "src/jennifer/server/resources/NotFound";
-                 absolutePath = new File(path).getAbsolutePath();
-                 rootPath = new File(absolutePath);
-                 System.out.println(rootPath);
+                path = "src/jennifer/server/resources/NotFound.txt.txt";
+                absolutePath = new File(path).getAbsolutePath();
+                rootPath = new File(absolutePath);
+                System.out.println(rootPath);
 
                 String contentLength = Long.toString(rootPath.length());
-                String[] headers = constructHeader("HTTP/1.1 404 NOT FOUND", contentLength);
+                String[] headers = constructHeader("HTTP/1.1 404 NOT FOUND", contentLength, fileExt);
                 sendResponse(headers, rootPath);
             }
-
         }
 
-        public String[] constructHeader(String statusCode, String contentLength){
+        public String[] constructHeader(String statusCode, String contentLength, String ext){
 
-            String[] headers = new String[6];
+            String[] headers = new String[8];
             headers[0] = statusCode + "\r\n";
-            headers[1]= "Date: Fri, 15 Mar 2019" + "\r\n";
+            headers[1]= "Date: Mon, 18 Mar 2019 17:00:00 GMT" + "\r\n";
             headers[2] = "Server: Phoenix Server" + "\r\n";
-            if(count == 3){
-                System.out.println(count);
+
+            if(ext.equals("png")){
+                System.out.println("This is an image");
                 headers[3] = "Content-Type: image/png" + "\r\n";
             }
-            else{
 
-                headers[3] = "Content-Type: text/html; text/css; charset=utf-8" + "\r\n";
+            else if (ext.equals("css")){
+                System.out.println("This is a css file");
+                headers[3] = "Content-Type: text/css charset=utf-8; " + "\r\n";
             }
-            headers[4] = "Content-Length: " + contentLength + "\r\n";
-            headers[5] = "\r\n";
+
+            else if (ext.equals("html")){
+                System.out.println("This is an html file");
+                headers[3] = "Content-Type: text/html charset=utf-8; " + "\r\n";
+            }
+
+            else {
+                headers[3] = "Content-Type: text/plain; charset=utf-8" + "\r\n";
+            }
+            headers[4] = "Content-Length: " + contentLength  + "\r\n";
+            headers[5] = "Connection: close "+ "\r\n";
+            headers[6] = "\r\n";
+            headers[7] = "\n\n";
 
             return headers;
 
         }
 
-
         public void sendResponse(String[] headers, File file){
 
-            FileInputStream fileReader = null;
-            DataOutputStream byteWriter = null;
-
-            try{
-
-                fileReader = new FileInputStream(file);
-                byteWriter = new DataOutputStream(connSocket.getOutputStream());
+            try (FileInputStream fileReader = new FileInputStream(file);
+                 DataOutputStream byteWriter = new DataOutputStream(connSocket.getOutputStream())){
 
                 //writing response headers to the connecting socket (client)
                 for(int i = 0; i < headers.length - 1; i++){
-
                     byteWriter.writeBytes(headers[i]);
                 }
 
@@ -139,15 +146,8 @@ public class Server {
                 int byteRead;
                 byte[] buffer = new byte[1048576];
                 while((byteRead = fileReader.read(buffer)) != -1){
-
-                    String s = new String(buffer);
-                    System.out.println(s);
-                    System.out.println(byteRead);
-
                     byteWriter.write(buffer, 0, byteRead);
-
                 }
-
 
             } catch (FileNotFoundException e){
                 System.out.println("File not found");
@@ -155,16 +155,6 @@ public class Server {
                 e.printStackTrace();
                 System.out.println("something went wrong while reading file");
             }
-
-            try{
-                fileReader.close();
-                byteWriter.close();
-            } catch(IOException e){
-                e.printStackTrace();
-            }
-
-
-
         }
 
     }
